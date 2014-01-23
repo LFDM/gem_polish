bad24cc534747d4204b53cec9baccbcb98eef8df
LFDM <1986gh@gmail.com>
2014-01-23 11:57:18 +0100

    Adds badge creation.

---
+++ lib/gem_polish/cli.rb
@@ -0,0 +1,52 @@
+require 'thor'
+
+class CLI < Thor
+  include Thor::Actions
+    def self.default_badges
+      %i{ badge_fury gemnasium travis coveralls code_climate }
+    end
+
+  desc "polish", "polishes your gem"
+  method_option :badges, type: :array, lazy_default: default_badges, aliases: '-b'
+  method_option :git_user_name, type: :string, aliases: '-g'
+  def polish
+    git_user_name = extract_git_user(options)
+    badges = options[:badges]
+
+    insert_badges(badges, git_user_name, gem_name) if badges
+  end
+
+
+  no_commands do
+    def gem_name
+      File.basename(Dir.pwd)
+    end
+
+
+    def extract_git_user(options)
+     user = options[:git_user_name] || `git config user.name`.chomp
+     user.empty? ? "TODO: Write your name" : user
+    end
+
+    BADGE_NAMES = {
+      badge_fury: 'Version',
+      gemnasium: 'Dependencies',
+      travis: 'Build Status',
+      coveralls: 'Coverage',
+      code_climate: 'Code Climate'
+    }
+
+    def insert_badges(badges, user, gem)
+      insert_into_file('README.md', after: /^#\s.*\n/, force: false) do
+        "\n#{badges.map { |badge| badge_link(badge, user, gem) }.join("\n")}\n"
+      end
+    end
+
+    def badge_link(badge, user, gem)
+      path = "http://allthebadges.io/#{user}/#{gem}/#{badge}"
+      "[![#{BADGE_NAMES[badge]}](#{path}.png)](#{path})"
+    end
+
+  end
+end
+
