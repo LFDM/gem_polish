886a108a930f9f83bbd4b5b3fe5bbd905244189a
LFDM <1986gh@gmail.com>
2014-01-23 18:42:42 +0100

    Kills the failing spec

--- spec/gem_polish_spec.rb
+++ spec/gem_polish_spec.rb
@@ -4,8 +4,4 @@ describe GemPolish do
   it 'should have a version number' do
     GemPolish::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
 end
