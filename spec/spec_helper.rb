067cd95b61eef18135c3831172dbc3daffca2d64
LFDM <1986gh@gmail.com>
2014-02-24 19:20:24 +0100

    Adds testfile helper methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,21 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'gem_polish'
+
+TESTFILES_DIR = "helpers/testfiles"
+
+def testfile(name)
+  File.expand_path("#{TESTFILES_DIR}/#{name}", __FILE__)
+end
+
+def read_testfile(name)
+  File.read(testfile(name))
+end
+
+def create_testfile(name)
+  `touch #{testfile(name)}`
+end
+
+def remove_testfiles(name)
+  `rm #{TESTFILES_DIR}/*`
+end
+
