31c0241cc3b86f1dfbc3cf4f477e9454e19998c9
LFDM <1986gh@gmail.com>
2014-02-24 22:29:57 +0100

    Set up Gem CLI

--- lib/gem_polish/gem.rb
+++ lib/gem_polish/gem.rb
@@ -1,17 +1,29 @@
 module GemPolish
   class Gem < Thor
+    require 'gem_polish/cli/gem_manipulator'
     namespace :gem
 
     desc 'add', 'adds one or several gems to your Gemfile'
-    def add(*var)
+    def add(*gems)
+      action(:add, gems, options)
     end
 
     desc 'delete', 'deletes one or several gems from your Gemfile'
-    def delete(var)
+    def delete(*gems)
+      action(:delete, gems, options)
     end
 
     desc 'update', 'updates one specific gem from your Gemfile'
-    def update(var)
+    def update(*gems)
+      action(:update, gems, options)
+    end
+
+    no_commands do
+      def action(name, options)
+        inside('.') do
+          GemManipulator.new(self).send(name, options)
+        end
+      end
     end
   end
 end
