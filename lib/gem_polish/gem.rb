8ce14f8df334d6540e929b45ad5f902f0dff67f0
LFDM <1986gh@gmail.com>
2014-03-08 18:58:17 +0100

    Fix action methods in Gem

--- lib/gem_polish/gem.rb
+++ lib/gem_polish/gem.rb
@@ -1,27 +1,30 @@
 module GemPolish
   class Gem < Thor
+    include Thor::Actions
+
     require 'gem_polish/cli/gem_manipulator'
+
     namespace :gem
 
     desc 'add', 'adds one or several gems to your Gemfile'
     def add(*gems)
-      action(:add, gems, options)
+      gem_action(:add, gems, options)
     end
 
     desc 'delete', 'deletes one or several gems from your Gemfile'
     def delete(*gems)
-      action(:delete, gems, options)
+      gem_action(:delete, gems, options)
     end
 
     desc 'update', 'updates one specific gem from your Gemfile'
     def update(*gems)
-      action(:update, gems, options)
+      gem_action(:update, gems, options)
     end
 
     no_commands do
-      def action(name, options)
+      def gem_action(name, gems, options)
         inside('.') do
-          GemManipulator.new(self).send(name, options)
+          CLI::GemManipulator.new(self).send(name, gems, options)
         end
       end
     end
